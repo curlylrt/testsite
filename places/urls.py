b0132f5c4cdeb8ab67435ce679ddb5522554fed8
Liso <yuechen1989@gmail.com>
2015-03-11 14:10:47 -0700

    mock up menu

--- places/urls.py
+++ places/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from places import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='places'),
     url(r'^(?P<place_id>\d+)/$', views.detail, name='detail'),
     )
--- trajectory/urls.py
+++ trajectory/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from trajectory import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='trajectory'),
     url(r'^partition', views.partition, name='partition'),
     url(r'^route', views.route, name='route'),
     url(r'^test', views.test, name='test'),
